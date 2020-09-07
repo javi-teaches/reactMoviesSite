55490eb05dc6bd5ba8de5189047a3c746e9886aa
japsolo <javier@adboxpublicidad.com>
2020-09-07 20:56:28 -0300

    2da clase

--- src/routes/api.js
+++ src/routes/api.js
@@ -4,10 +4,18 @@ const router = express.Router();
 const { Movie } = require('../database/models');
 
 router.get('/', (req, res) => {
-	Movie.findAll()
+	Movie.findAll({
+		order: [
+			['title', 'DESC']
+		]
+	})
 		.then(movies => {
 			res.send(movies);
 		});
 });
+
+router.post('', (req, res) => {
+	res.send('Ok');
+})
 
 module.exports = router;
